4e82d9cc07929659254c7051ad02528baa3498ae
Jane <velick@list.ru>
2024-02-10 21:12:25 +0700

    Локалированы формы входа и регистрации Значения API_URL берутся из переменных окружения

--- src/frontend/src/http/index.ts
+++ src/frontend/src/http/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const $api = axios.create({
     withCredentials: true,
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.API_URL,
     timeout: 1000
 
 })
