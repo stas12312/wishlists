d4d5e960159bfe16ca8c14b4c9cca408ca863163
stas12312 <i@svrush.ru>
2024-03-10 17:25:30 +0700

    [UI] Переадресация с главной страницы на страницу с вишлистами Resolve #118

--- src/frontend/src/pages/Welcome.tsx
+++ src/frontend/src/pages/Welcome.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
-import { createTheme } from '@mui/material/styles';
+import {createTheme} from '@mui/material/styles';
 import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import {Navigate} from "react-router-dom";
 
 const defaultTheme = createTheme();
 
 function Welcome() {
+    const {store} = useContext(Context);
+
+    if (store.isAuth) {
+        return (
+            <Navigate replace to={"/wishlists"}/>
+        )
+    }
 
     return (
         <div> {process.env.REACT_APP_API_URL}</div>
