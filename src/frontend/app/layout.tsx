08b37de1a0f589f4aec0c421bc6c33b17adb6856
Stanislav Rush <i@svrush.ru>
2024-12-06 21:32:09 +0700

    [UI] Исправлена иконка

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -33,7 +33,6 @@ export default async function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <head title="Мои желания">
-        <link rel="shortcut icon" href="/favicon.png" />
         <meta
           content="width=device-width, initial-scale=1, user-scalable=1, minimum-scale=1, maximum-scale=5"
           name="viewport"
