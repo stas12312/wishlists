f51f1d42c7aa93c675d1b4313df91ff9f893b4be
Stanislav Rush <i@svrush.ru>
2024-12-26 14:02:43 +0700

    [UI] Добавлено количества желаний Resolve #266

---
+++ src/frontend/lib/label.ts
@@ -0,0 +1,4 @@
+export function getLabelForCount(count: number, labels: string[]): string {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return labels[count % 100 > 4 && count % 100 < 20 ? 2 : cases[count % 10 < 5 ? count % 10 : 5]];
+}
