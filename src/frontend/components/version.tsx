9467b1f292281e5d87b534845bcd94a0f57b0c34
Stanislav Rush <i@svrush.ru>
2024-12-27 15:59:52 +0700

    [UI] Убран префикс v при отображении версии Resolve #289

--- src/frontend/components/version.tsx
+++ src/frontend/components/version.tsx
@@ -5,11 +5,10 @@ const Version = () => {
       target="_blank"
       className="cursor-default hover:text-default-400 block text-center text-default-300"
     >
-      <small className="block m-0">{`v${process.env.VERSION ?? process.env.npm_package_version}`}</small>
+      <small className="block m-0">{`${process.env.VERSION ?? process.env.npm_package_version}`}</small>
       <small className="block m-0">{process.env.BUILD_TIME} </small>
     </a>
   );
 };
 
 export default Version;
-
