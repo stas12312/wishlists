b1cca10e4146ae2495a85eafd1359cbae30aa65b
Stanislav Rush <i@svrush.ru>
2025-01-10 01:41:42 +0700

    [UI] Добавлены метаданные для страницы вишлиста

--- src/frontend/app/(root)/wishlists/[uuid]/page.tsx
+++ src/frontend/app/(root)/wishlists/[uuid]/page.tsx
@@ -1,4 +1,28 @@
+import { Metadata } from "next";
+
 import Wishes from "@/components/wishlistDetail";
+import { getWishlist } from "@/lib/requests";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ uuid: string }>;
+}): Promise<Metadata> {
+  const uuid = (await params).uuid;
+
+  const wishlist = await getWishlist(uuid);
+  if ("message" in wishlist) {
+    return {};
+  }
+
+  return {
+    title: `Вишлист - ${wishlist.name}`,
+    openGraph: {
+      title: wishlist.name,
+      description: wishlist.description,
+    },
+  };
+}
 
 export default async function Page({
   params,
--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -11,7 +11,7 @@ import { siteConfig } from "@/config/site";
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
+    template: `%s`,
   },
   description: siteConfig.description,
   icons: {
