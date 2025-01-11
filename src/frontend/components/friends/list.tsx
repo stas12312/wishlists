d1ec41d6e70e803f38af57951e2a5df384323598
Stanislav Rush <i@svrush.ru>
2025-01-11 22:13:40 +0700

    [UI] Исправлены отступы в списке друзей Resolve #371

--- src/frontend/components/friends/list.tsx
+++ src/frontend/components/friends/list.tsx
@@ -93,7 +93,7 @@ const FriendsList = observer(() => {
   ));
   if (friends) {
     return (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {users}
       </div>
     );
