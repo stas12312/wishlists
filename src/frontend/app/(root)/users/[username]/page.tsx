8d79ef6871003fa01d52e4b18d15bf09d4bedefc
Stanislav Rush <i@svrush.ru>
2025-01-12 23:04:36 +0700

    [UI] Добавлены заголовки и описания страниц

--- src/frontend/app/(root)/users/[username]/page.tsx
+++ src/frontend/app/(root)/users/[username]/page.tsx
@@ -1,4 +1,28 @@
+import { Metadata } from "next";
+
 import UserView from "@/components/userPage";
+import { getUserByUsername } from "@/lib/requests";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ username: string }>;
+}): Promise<Metadata> {
+  const username = (await params).username;
+
+  const user = await getUserByUsername(username);
+  if ("message" in user) {
+    return {};
+  }
+
+  return {
+    title: user.name,
+    openGraph: {
+      title: user.name,
+      description: `${user.name} вишлисты пользователя`,
+    },
+  };
+}
 
 const UserPage = async ({
   params,
