06725b9e7aa79a874c567df04bdfa5bbb46ec9c9
Stanislav Rush <i@svrush.ru>
2025-01-17 21:36:20 +0700

    [UI] Добавлена генерация OG:Image для основных страниц (#415)
    Resolve #404

---
+++ src/frontend/app/(root)/users/[username]/opengraph-image.tsx
@@ -0,0 +1,16 @@
+import { getOgImage } from "@/components/ogImage";
+import { getUserByUsername } from "@/lib/requests";
+
+export const runtime = "edge";
+export const alt = "Вишлист";
+
+export const contentType = "image/png";
+
+export default async function Image({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getUserByUsername(params.username);
+  return await getOgImage(user.name, "", "Профиль пользователя");
+}
