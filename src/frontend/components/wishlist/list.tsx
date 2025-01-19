be7fc7c3f6348964911bfaad7563c991a3536135
Stanislav Rush <i@svrush.ru>
2025-01-19 21:35:22 +0700

    [UI] Доработано отображение карточек вишлистов (#448)
    Resolve #447

--- src/frontend/components/wishlist/list.tsx
+++ src/frontend/components/wishlist/list.tsx
@@ -128,7 +128,7 @@ export const Wishlists = observer(
     }, [lastItem]);
 
     return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
         {actions.filter ? (
           <span className="flex col-span-full">
             <WishlistFilter filter={filter} setFilter={setFilter} />
