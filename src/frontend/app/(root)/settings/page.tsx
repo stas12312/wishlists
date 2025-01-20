842462c4479dc4ed7b247986fcf65af82a3e9def
Stanislav Rush <i@svrush.ru>
2025-01-21 01:25:24 +0700

    [UI] Исправлено отображение копирование ссылки на профиль
    Resolve #467

--- src/frontend/app/(root)/settings/page.tsx
+++ src/frontend/app/(root)/settings/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 import { observer } from "mobx-react-lite";
 import toast from "react-hot-toast";
-import { MdLink, MdOutlineExitToApp } from "react-icons/md";
+import { MdLink, MdOutlineExitToApp, MdContentCopy } from "react-icons/md";
 
 import userStore from "@/store/userStore";
 import { getUserLink } from "@/lib/label";
@@ -29,16 +29,17 @@ const Profile = observer(() => {
         <p className="text-xl">Профиль</p>
         <div className="flex flex-col gap-1 rounded-xl bg-content1 box-border shadow-medium p-4 m-1">
           <p className="">Ссылка на профиль</p>
-          <div className="flex gap-2">
+          <div className="flex gap-2 flex-col md:flex-row">
             <Input
               isReadOnly
+              className="w-full md:max-w-[60%]"
               labelPlacement="outside"
               startContent={<MdLink />}
               value={userStore.user.id ? profileLink : ""}
             />
             <Button
-              color="primary"
-              // startContent={<MdOutlineContentCopy />}
+              className="w-full md:w-[40%]"
+              startContent={<MdContentCopy />}
               onPress={() => {
                 navigator.clipboard.writeText(profileLink);
                 toast.success("Ссылка на профиль скопирована");
