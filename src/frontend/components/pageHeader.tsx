db0446adc32b151915e16bff0a99fd5737f76fc8
Stanislav Rush <i@svrush.ru>
2025-01-21 00:08:07 +0700

    [UI] Переработано меню (#453)
    Resolve #452

--- src/frontend/components/pageHeader.tsx
+++ src/frontend/components/pageHeader.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 const PageHeader = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 mt-4 md:mt-0">
       <h1 className="text-2xl col-span-full text-center lg:text-left">
         {children}
       </h1>
