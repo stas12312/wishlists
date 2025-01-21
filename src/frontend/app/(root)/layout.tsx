565c5a01d1265c6d0ac3b8ee05fe4b4ccd3d5bac
Stanislav Rush <i@svrush.ru>
2025-01-22 00:31:08 +0700

    [UI] Меню зафиксировано по ширине
    Resolve #484

--- src/frontend/app/(root)/layout.tsx
+++ src/frontend/app/(root)/layout.tsx
@@ -15,41 +15,40 @@ export default async function Layout({
   const cookieStore = await cookies();
   const isLogin = cookieStore.has("access_token");
   return (
-    <div className="min-h-screen justify-between flex-col flex">
-      <div className="grid grid-cols-12 gap-1 pb-4">
-        {!isLogin ? (
-          <div className="flex col-span-full">
-            <div className="ml-auto mr-6 py-2 flex flex-row gap-2">
+    <div className="flex flex-col  min-h-screen justify-between">
+      <div>
+        <header className="w-full flex col-span-full justify-end">
+          {isLogin ? (
+            <div className="ml-auto mr-6 pt-3 px-10 flex flex-row gap-2">
+              <ThemeSwitcher />
+              <UserItem />
+            </div>
+          ) : (
+            <div className="ml-auto mr-6 py-2 flex gap-2">
               <ThemeSwitcher />
               <Login />
             </div>
-          </div>
-        ) : (
-          <>
-            <div className="flex col-span-full justify-end">
-              <div className="ml-auto mr-6 pt-3 px-10 flex flex-row gap-2">
-                <ThemeSwitcher />
-                <UserItem />
-              </div>
-            </div>
-            <div className="col-span-full md:col-span-3 lg:col-span-2 flex-col">
-              <div className="sticky top-10 hidden md:block px-4">
-                <Menu variant="desktop" />
-                <span className="hidden md:block">
+          )}
+        </header>
+
+        <div className="gap-1 pb-4 flex">
+          {isLogin ? (
+            <>
+              <div className="hidden md:block col-span-full flex-col w-[240px]">
+                <div className="sticky top-10 hidden md:block px-4">
+                  <Menu variant="desktop" />
                   <footer className="px-1">
                     <Divider />
                     <Contacts />
                     <Version />
                   </footer>
-                </span>
+                </div>
               </div>
-            </div>
-          </>
-        )}
-        <div
-          className={`col-span-full ${isLogin ? "md:col-span-9 lg:col-span-10" : null} px-1 md:px-4 lg:px-4`}
-        >
-          <main>{children}</main>
+            </>
+          ) : null}
+          <div className={`px-3 md:px-4 lg:px-4 w-full`}>
+            <main>{children}</main>
+          </div>
         </div>
       </div>
       <span className={`"mt-auto" ${isLogin ? "md:hidden" : ""}`}>
