9dff572b39f47b8361dc44099b9ed1fe60c489a1
Stanislav Rush <i@svrush.ru>
2025-01-23 22:53:46 +0700

    [UI] Исправлено отображение ошибок при загрузке изображения (#507)
    Resolve #502
    Resolve #501

--- src/frontend/components/uploadButton.tsx
+++ src/frontend/components/uploadButton.tsx
@@ -1,33 +1,58 @@
 import { useId, useState } from "react";
 import { Image } from "@nextui-org/image";
 import { MdOutlineFileUpload } from "react-icons/md";
+import { Spinner } from "@nextui-org/spinner";
 
 import { uploadFile } from "@/lib/requests";
+
+const FILE_SIZE_LIMIT = 3 * 1024 * 1024; // 3 МБ
+const FILE_SIZE_NAME = "3 МБ";
+
 const UploadButton = ({
   onUpload,
   previewUrl,
   className,
   accept,
+  onError,
 }: {
   onUpload: { (url: string): void };
   previewUrl: string | undefined;
   className: string;
   accept: string[];
+  onError: { (error: string): void };
 }) => {
   const [isOver, setIsOver] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   function isValidFile(file: File): boolean {
-    const parts = file.name.split(".");
-    const ext = parts[parts.length - 1];
+    const ext = getFileExt(file.name);
     return accept.indexOf(ext) !== -1;
   }
 
+  function getFileExt(filename: string): string {
+    const parts = filename.split(".");
+    return parts[parts.length - 1];
+  }
+
   async function handleFile(file: File) {
-    const response = await uploadFile(file);
+    onError("");
     if (!isValidFile(file)) {
+      onError(`Неподдерживаемый тип файла .${getFileExt(file.name)}`);
       return;
     }
-    onUpload(response);
+    if (file.size > FILE_SIZE_LIMIT) {
+      onError(`Размер файла превышает допустимый размер (${FILE_SIZE_NAME})`);
+      return;
+    }
+    try {
+      setIsLoading(true);
+      const response = await uploadFile(file);
+      onUpload(response);
+    } catch {
+      onError("Возникла ошибка");
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   async function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -52,7 +77,6 @@ const UploadButton = ({
   function onDragLeave(e: React.DragEvent<HTMLLabelElement>) {
     e.preventDefault();
     e.stopPropagation();
-
     setIsOver(false);
   }
 
@@ -69,31 +93,42 @@ const UploadButton = ({
       onDragOver={onDragEnter}
       onDrop={onDrop}
     >
-      <Image className={className + " z-2"} src={previewUrl || undefined} />
-      {!isOver ? (
-        <span className="group-hover:opacity-90 w-full h-full duration-300 absolute flex top-0 justify-center hover:bg-default-300 bg-default-200 opacity-40 rounded-xl z-1">
-          <span className="absolute flex flex-col justify-center left-0 top-0 right-0 bottom-0 ">
-            <span className="flex justify-center text-2xl">
-              <MdOutlineFileUpload />
-            </span>
-            <span className="text-tiny text-center font-normal">загрузить</span>
-          </span>
-        </span>
+      {isLoading ? (
+        <div className="w-full h-full flex justify-center">
+          <Spinner />
+        </div>
       ) : (
-        <span className="group-hover:opacity-90 w-full h-full duration-300 absolute flex top-0 justify-center hover:bg-default-300 bg-default-200 opacity-40 rounded-xl z-1" />
-      )}
+        <>
+          <Image className={className + " z-2"} src={previewUrl || undefined} />
 
-      <input
-        accept={accept
-          .map((value) => {
-            return `.${value}`;
-          })
-          .join(",")}
-        className="hidden"
-        id={id}
-        type="file"
-        onChange={handleFileChange}
-      />
+          {!isOver ? (
+            <span className="group-hover:opacity-90 w-full h-full duration-300 absolute flex top-0 justify-center hover:bg-default-300 bg-default-200 opacity-40 rounded-xl z-1">
+              <span className="absolute flex flex-col justify-center left-0 top-0 right-0 bottom-0 ">
+                <span className="flex justify-center text-2xl">
+                  <MdOutlineFileUpload />
+                </span>
+                <span className="text-tiny text-center font-normal">
+                  загрузить
+                </span>
+              </span>
+            </span>
+          ) : (
+            <span className="group-hover:opacity-90 w-full h-full duration-300 absolute flex top-0 justify-center hover:bg-default-300 bg-default-200 opacity-40 rounded-xl z-1" />
+          )}
+
+          <input
+            accept={accept
+              .map((value) => {
+                return `.${value}`;
+              })
+              .join(",")}
+            className="hidden"
+            id={id}
+            type="file"
+            onChange={handleFileChange}
+          />
+        </>
+      )}
     </label>
   );
 };
