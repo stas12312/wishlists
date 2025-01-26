b4ab0f6a147e8c99939f9fc2bc84d1bfb58f51f8
Stanislav Rush <i@svrush.ru>
2025-01-27 00:58:22 +0700

    [UI] Доработан manifest (#544)
    Resolve #543

---
+++ src/frontend/app/icon.tsx
@@ -0,0 +1,33 @@
+import { ImageResponse } from "next/og";
+import { BsStars } from "react-icons/bs";
+
+// Image metadata
+export const size = {
+  width: 144,
+  height: 144,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 144,
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+        }}
+      >
+        <BsStars style={{ color: "gold" }} />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+}
