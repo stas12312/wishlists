77d4253b1676a3086a018622d1f99c25d06bd124
Stanislav Rush <i@svrush.ru>
2025-02-02 21:38:58 +0700

    [UI] Доработано отображение форм авторизаций/регистраций на мобильных устройствах (#575)

--- src/frontend/app/auth/layout.tsx
+++ src/frontend/app/auth/layout.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-screen flex items-center w-screen justify-center">
-      <div className="w-full max-w-xl p-2">{children}</div>
+    <div className="md:h-screen flex items-center w-screen justify-center">
+      <div className="w-full max-w-xl pt-10 md:pt-0 p-2">{children}</div>
     </div>
   );
 }
