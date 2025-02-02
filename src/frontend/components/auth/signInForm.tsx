07f4647049b72d2a3e0b6da09e21dc559f81669a
Stanislav Rush <i@svrush.ru>
2025-02-02 20:14:54 +0700

    [UI] Доработаны формы авторизации/регистрации (#573)

--- src/frontend/components/auth/signInForm.tsx
+++ src/frontend/components/auth/signInForm.tsx
@@ -41,70 +41,72 @@ const SignInForm = ({ providers }: { providers: IOAuthProvider[] }) => {
     setFormData({ ...formData, [name]: value });
   };
   return (
-    <form
-      className="flex flex-col gap-2 bg-content1 p-4 rounded-xl box-border shadow-medium"
-      id="login"
-      onSubmit={handleSubmit}
-    >
-      <h2 className="text-center text-2xl">Войти</h2>
-      <div>
-        <Input
-          fullWidth
-          required
-          className="text-2xl"
-          label="Email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <PasswordInput
-          label="Пароль"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <small>
-          <Link className="text-sm" href="/auth/reset-password">
-            Забыли пароль?
-          </Link>
-        </small>
-      </div>
-      <span className="text-danger text-sm text-center">{errorMessage}</span>
-      <div>
-        <Button
-          fullWidth
-          isLoading={isLoading}
-          spinnerPlacement="end"
-          type="submit"
-        >
-          Войти
-        </Button>
-      </div>
-      <p className="text-center">
-        <Link href="/auth/register">Регистрация</Link>
-      </p>
-      <span className="grid grid-cols-5 md:grid-cols-4 gap-1">
-        <Divider className="my-auto col-span-1" />
-        <span className="text-center col-span-3 md:col-span-2">
-          или войти с помощью
+    <>
+      <form
+        className="flex flex-col gap-2 bg-content1 p-4 rounded-xl box-border shadow-medium"
+        id="login"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-center text-2xl">Вход в аккаунт</h2>
+        <div>
+          <Input
+            fullWidth
+            required
+            className="text-2xl"
+            label="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <PasswordInput
+            label="Пароль"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <small>
+            <Link className="text-sm" href="/auth/reset-password">
+              Забыли пароль?
+            </Link>
+          </small>
+        </div>
+        <span className="text-danger text-sm text-center">{errorMessage}</span>
+        <div>
+          <Button
+            fullWidth
+            isLoading={isLoading}
+            spinnerPlacement="end"
+            type="submit"
+          >
+            Войти
+          </Button>
+        </div>
+        <span className="grid grid-cols-5 md:grid-cols-4 mt-2">
+          <Divider className="my-auto col-span-1" />
+          <span className="text-center col-span-3 md:col-span-2">
+            или войти с помощью
+          </span>
+          <Divider className="my-auto col-span-1" />
         </span>
-        <Divider className="my-auto col-span-1" />
-      </span>
 
-      <div className="flex justify-center">
-        {providers.map((provider) => {
-          return (
-            <div key={provider.name} className="p-1 rounded-full">
-              <a className="h-10 text-5xl font-bold" href={provider.url}>
-                {provider.icon}
-              </a>
-            </div>
-          );
-        })}
-      </div>
-    </form>
+        <div className="flex justify-center">
+          {providers.map((provider) => {
+            return (
+              <div key={provider.name} className="p-1 rounded-full">
+                <a className="h-10 text-5xl font-bold" href={provider.url}>
+                  {provider.icon}
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </form>
+      <p className="text-center mt-4">
+        Нет аккаунта? <Link href="/auth/register">Зарегистрироваться</Link>
+      </p>
+    </>
   );
 };
 
