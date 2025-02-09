bbc745d0b227de455973557121843e5228a9dd7f
Stanislav Rush <i@svrush.ru>
2025-02-09 13:17:49 +0700

    [API] Добавлена отправка Content-Type при загрузке в S3 хранилище Resolve #607

--- src/backend/repository/impl/image.go
+++ src/backend/repository/impl/image.go
@@ -7,6 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"io"
 	"main/config"
+	"mime"
+	"path/filepath"
 	"strings"
 )
 
@@ -29,12 +31,12 @@ type S3ImageRepository struct {
 
 func (r *S3ImageRepository) Upload(filename string, file io.Reader) (string, error) {
 	uploader := s3manager.NewUploader(r.Session)
-
 	_, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket:       aws.String(r.Bucket),
 		Key:          aws.String(filename),
 		Body:         file,
 		CacheControl: aws.String("max-age=15552000,immutable"),
+		ContentType:  aws.String(mime.TypeByExtension(filepath.Ext(filename))),
 	})
 	return strings.Join([]string{r.Domain, filename}, "/"), err
 
