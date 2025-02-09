e0d55727146bc3b455bbb57a882aa3e0a9b3a429
Stanislav Rush <i@svrush.ru>
2025-02-09 21:03:01 +0700

    [UI] Настроен Sentry Resolve #617

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -9,3 +9,21 @@ const nextConfig = {
 
 
 module.exports = nextConfig
+
+
+// Injected content via Sentry wizard below
+
+const { withSentryConfig } = require("@sentry/nextjs");
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+    hideSourceMaps: true,
+    disableLogger: true,
+  }
+);
