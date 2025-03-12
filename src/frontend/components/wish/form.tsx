d4ec95bb9ceb7528f5e1ba9260939bc0354b09bb
Stanislav Rush <i@svrush.ru>
2025-03-13 01:51:54 +0700

    [UI] Добавлена возможность добавлять желание по ссылке

--- src/frontend/components/wish/form.tsx
+++ src/frontend/components/wish/form.tsx
@@ -3,10 +3,15 @@ import { Button } from "@heroui/button";
 import { Input } from "@heroui/input";
 import { ClipboardEvent, FormEvent, useState } from "react";
 import { Form } from "@heroui/form";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { useDisclosure } from "@heroui/modal";
+import { Divider } from "@heroui/divider";
 
 import UploadButton from "../uploadButton";
 import Desirability from "../desirability";
 import MarketIcon from "../marketIcon";
+
+import LoadByLinkModal from "./loadByLinkModal";
 
 import { uploadFile } from "@/lib/requests";
 import { updateWish } from "@/lib/requests/wish";
@@ -15,6 +20,7 @@ import { IError } from "@/lib/models";
 import { IWish } from "@/lib/models/wish";
 import { isURL } from "@/lib/url";
 import { checkFile } from "@/lib/file";
+import { ParseResult } from "@/lib/models/parse";
 
 const ACCEPTED_FILE_EXTS = ["jpg", "jpeg", "png", "webp"];
 
@@ -55,6 +61,7 @@ export default function WishForm(props: {
 
   const [isCreating, setIsCreating] = useState(false);
   const [imageIsLoading, setImageIsLoading] = useState(false);
+  const loadByLinkDisclosure = useDisclosure();
 
   async function handleFile(file: File) {
     setErrorMessages({ ...errorMessages, image: "" });
@@ -134,112 +141,164 @@ export default function WishForm(props: {
     }
   }
 
+  function setLinkResult(result: ParseResult, link: string) {
+    setFormData({
+      ...formData,
+      link: link,
+      name: result.title,
+      image: result.image,
+      cost: result.cost,
+    });
+  }
+
   return (
-    <div onPaste={onPasteImage}>
-      <Form
-        className="flex flex-col gap-3"
-        id="wish"
-        validationBehavior="native"
-        onSubmit={onSumbitForm}
-      >
-        <Input
-          isClearable
-          isRequired
-          errorMessage={errorMessages.Name}
-          label="Название"
-          name="name"
-          validate={(value) => {
-            if (value === "") {
-              return "Заполните это поле";
-            }
-            if (value.length > 50) {
-              return "Максимальна длина строки 50 символов";
-            }
-            return null;
-          }}
-          value={formData.name}
-          onChange={handlerChange}
-          onClear={() => setFormData({ ...formData, name: "" })}
-        />
-        <Input
-          isClearable
-          label="Комментарий"
-          name="comment"
-          value={formData.comment}
-          onChange={handlerChange}
-          onClear={() => setFormData({ ...formData, comment: "" })}
-        />
-        <Input
-          isClearable
-          label="Цена"
-          name="cost"
-          value={formData.cost !== 0 ? formData.cost?.toLocaleString() : ""}
-          onChange={handlerChange}
-          onClear={() => setFormData({ ...formData, cost: undefined })}
-        />
-        <div className="flex w-full gap-4">
+    <>
+      <div onPaste={onPasteImage}>
+        {props.wish === undefined ? (
+          <>
+            <Button
+              fullWidth
+              className=""
+              color="primary"
+              endContent={
+                <div className="flex gap-1">
+                  <MarketIcon link={"ozon.ru"} />
+                  <MarketIcon link={"wildberries.ru"} />
+                </div>
+              }
+              startContent={<MdOutlineFileDownload />}
+              variant="flat"
+              onPress={() => {
+                loadByLinkDisclosure.onOpen();
+              }}
+            >
+              Загрузить по ссылке
+            </Button>
+            <span className="grid grid-cols-5 md:grid-cols-4 my-3">
+              <Divider className="my-auto col-span-1" />
+              <span className="text-center col-span-3 md:col-span-2">
+                или заполнить вручную
+              </span>
+              <Divider className="my-auto col-span-1" />
+            </span>
+          </>
+        ) : null}
+        <Form
+          className="flex flex-col gap-3"
+          id="wish"
+          validationBehavior="native"
+          onSubmit={onSumbitForm}
+        >
           <Input
             isClearable
-            label="Ссылка на товар"
-            name="link"
+            isRequired
+            errorMessage={errorMessages.Name}
+            label="Название"
+            name="name"
             validate={(value) => {
-              if (value.length > 500) {
-                return "Максимальна длина строки 500 символов";
+              if (value === "") {
+                return "Заполните это поле";
               }
-              if (value && !isURL(value)) {
-                return "Некорректная ссылка";
+              if (value.length > 50) {
+                return "Максимальна длина строки 50 символов";
               }
               return null;
             }}
-            value={formData.link}
+            value={formData.name}
             onChange={handlerChange}
-            onClear={() => setFormData({ ...formData, link: "" })}
+            onClear={() => setFormData({ ...formData, name: "" })}
           />
-          {<MarketIcon className="my-auto" link={formData.link} />}
-        </div>
-
-        <div className="flex sm:flex-row flex-col gap-4 w-full">
           <Input
             isClearable
-            label="Ссылка на картинку"
-            name="image"
-            value={formData.image}
+            label="Комментарий"
+            name="comment"
+            value={formData.comment}
             onChange={handlerChange}
-            onClear={() => setFormData({ ...formData, image: "" })}
-            onPaste={onPasteImage}
+            onClear={() => setFormData({ ...formData, comment: "" })}
           />
-          <div className="flex flex-col">
-            <UploadButton
-              accept={ACCEPTED_FILE_EXTS}
-              className="h-[140px] w-[180px] object-cover"
-              handleFile={handleFile}
-              isLoading={imageIsLoading}
-              previewUrl={formData.image}
+          <Input
+            isClearable
+            label="Цена"
+            name="cost"
+            value={formData.cost !== 0 ? formData.cost?.toLocaleString() : ""}
+            onChange={handlerChange}
+            onClear={() => setFormData({ ...formData, cost: undefined })}
+          />
+          <div className="flex w-full gap-4">
+            <Input
+              isClearable
+              label="Ссылка на товар"
+              name="link"
+              validate={(value) => {
+                if (value.length > 500) {
+                  return "Максимальна длина строки 500 символов";
+                }
+                if (value && !isURL(value)) {
+                  return "Некорректная ссылка";
+                }
+                return null;
+              }}
+              value={formData.link}
+              onChange={handlerChange}
+              onClear={() => setFormData({ ...formData, link: "" })}
             />
-            <span className="text-danger text-tiny">{errorMessages.image}</span>
+            {<MarketIcon className="my-auto" link={formData.link} />}
           </div>
-        </div>
-        <div className="flex flex-col justify-center items-center w-full">
-          <span>Желанность</span>
-          <Desirability
-            size="xl"
-            value={formData.desirability}
-            onChange={(value) => {
-              setFormData({ ...formData, desirability: value });
-            }}
-          />
-        </div>
-        {errorMessages.details ? (
-          <p className="text-danger text-tiny">{errorMessages.details}</p>
-        ) : (
-          <span />
-        )}
 
-        <Button fullWidth isLoading={isCreating} type="submit">
-          Сохранить
-        </Button>
-      </Form>
-    </div>
+          <div className="flex sm:flex-row flex-col gap-4 w-full">
+            <Input
+              isClearable
+              label="Ссылка на картинку"
+              name="image"
+              value={formData.image}
+              onChange={handlerChange}
+              onClear={() => setFormData({ ...formData, image: "" })}
+              onPaste={onPasteImage}
+            />
+            <div className="flex flex-col">
+              <UploadButton
+                accept={ACCEPTED_FILE_EXTS}
+                className="h-[140px] w-[180px] object-cover"
+                handleFile={handleFile}
+                isLoading={imageIsLoading}
+                previewUrl={formData.image}
+              />
+              <span className="text-danger text-tiny">
+                {errorMessages.image}
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center w-full">
+            <span>Желанность</span>
+            <Desirability
+              size="xl"
+              value={formData.desirability}
+              onChange={(value) => {
+                setFormData({ ...formData, desirability: value });
+              }}
+            />
+          </div>
+          {errorMessages.details ? (
+            <p className="text-danger text-tiny">{errorMessages.details}</p>
+          ) : (
+            <span />
+          )}
+
+          <Button fullWidth isLoading={isCreating} type="submit">
+            Сохранить
+          </Button>
+        </Form>
+      </div>
+      <div>
+        {loadByLinkDisclosure.isOpen ? (
+          <LoadByLinkModal
+            isOpen={loadByLinkDisclosure.isOpen}
+            onLinkLoad={setLinkResult}
+            onOpenChange={loadByLinkDisclosure.onOpenChange}
+          />
+        ) : null}
+      </div>
+    </>
   );
 }
 
