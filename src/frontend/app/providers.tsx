5fd2e8696025fd300e0715d160dfda9b82e3a517
Stanislav Rush <i@svrush.ru>
2025-03-13 00:00:05 +0700

    [UI] Исправлено отображение уведомлений (#693)
    Resolve #688

--- src/frontend/app/providers.tsx
+++ src/frontend/app/providers.tsx
@@ -5,6 +5,7 @@ import { HeroUIProvider } from "@heroui/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ToastProvider } from "@heroui/toast";
+import { useCallback } from "react";
 
 type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
@@ -16,15 +17,23 @@ export interface ProvidersProps {
 export function Providers({ children }: ProvidersProps) {
   const router = useRouter();
 
+  const [width, setWidth] = React.useState(0);
+
+  const handleWindowResize = useCallback(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  React.useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, [handleWindowResize]);
+
   return (
     <HeroUIProvider navigate={router.push}>
-      <ToastProvider
-        toastProps={{
-          classNames: {
-            base: "bottom-20 md:bottom-2",
-          },
-        }}
-      />
+      <ToastProvider toastOffset={width < 768 ? 80 : 0} />
       <NextThemesProvider attribute="class" defaultTheme="light">
         {children}
       </NextThemesProvider>
