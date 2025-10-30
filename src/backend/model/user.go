1935e085b8be5b9322a9cb93af0580420198e5fe
Stanislav Rush <i@svrush.ru>
2025-10-30 22:27:09 +0700

    [API] Исправлено получение NULL значения из поля email (#818)
    Resolve #814

--- src/backend/model/user.go
+++ src/backend/model/user.go
@@ -1,15 +1,16 @@
 package model
 
 type User struct {
-	Id        int64    `json:"id" db:"user_id"`
-	Email     string   `json:"email" db:"pretty_email"`
-	Name      string   `json:"name"`
-	Password  string   `json:"-"`
-	IsActive  bool     `json:"-" db:"is_active"`
-	Username  string   `json:"username" db:"username"`
-	Image     string   `json:"image" db:"image"`
-	CreatedAt string   `json:"-" db:"created_at"`
-	Birthday  NullDate `json:"birthday" db:"birthday"`
+	Id        int64      `json:"id" db:"user_id"`
+	Email     string     `json:"email" db:"pretty_email"`
+	Name      string     `json:"name"`
+	Password  string     `json:"-"`
+	IsActive  bool       `json:"-" db:"is_active"`
+	Username  string     `json:"username" db:"username"`
+	Image     string     `json:"image" db:"image"`
+	CreatedAt string     `json:"-" db:"created_at"`
+	Birthday  NullDate   `json:"birthday" db:"birthday"`
+	RawEmail  NullString `json:"-" db:"email"`
 }
 
 type UserForUpdate struct {
