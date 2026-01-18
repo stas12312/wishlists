8f1f89a6cc11d975bd4e8a49912dccf4fa0dbeb5
Stanislav Rush <i@svrush.ru>
2026-01-18 17:24:22 +0700

    [UI] Добавлен логотип сервиса над меню Resolve #810

---
+++ src/frontend/components/logo.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { BsStars } from "react-icons/bs";
+
+export const Logo = () => {
+  return (
+    <Link
+      className="flex font-bold font-mono text-2xl gap-1 justify-center text-center items-center"
+      href="/"
+    >
+      <BsStars style={{ color: "gold" }} />
+      <p className="text-bold">MyWishlists</p>
+    </Link>
+  );
+};
