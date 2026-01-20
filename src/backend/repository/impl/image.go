d332df1e4495ea6d97693ae7bc033d9bacd2ef42
Stanislav Rush <i@svrush.ru>
2026-01-21 02:34:19 +0700

    [API] Переход на aws-sdk-go-v2 Resolve #861

--- src/backend/repository/impl/image.go
+++ src/backend/repository/impl/image.go
@@ -1,37 +1,52 @@
 package impl
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"context"
 	"io"
 	"main/config"
 	"mime"
 	"path/filepath"
 	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	s3Config "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func NewS3ImageRepository(config *config.S3Config) *S3ImageRepository {
-	s, _ := session.NewSession(
-		&aws.Config{
-			Region:      &config.Region,
-			Credentials: credentials.NewStaticCredentials(config.AccessKey, config.SecretAccessKey, ""),
-			Endpoint:    &config.EndpointUrl,
-		})
+	cfg, err := s3Config.LoadDefaultConfig(
+		context.TODO(),
+		s3Config.WithRegion(config.Region),
+		s3Config.WithCredentialsProvider(
+			credentials.NewStaticCredentialsProvider(
+				config.AccessKey,
+				config.SecretAccessKey,
+				"",
+			),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
 
-	return &S3ImageRepository{s, config.Bucket, config.Domain}
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String(config.EndpointUrl)
+	})
+
+	return &S3ImageRepository{client, config.Bucket, config.Domain}
 }
 
 type S3ImageRepository struct {
-	*session.Session
+	*s3.Client
 	Bucket string
 	Domain string
 }
 
 func (r *S3ImageRepository) Upload(filename string, file io.Reader) (string, error) {
-	uploader := s3manager.NewUploader(r.Session)
-	_, err := uploader.Upload(&s3manager.UploadInput{
+	uploader := manager.NewUploader(r.Client)
+	_, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
 		Bucket:       aws.String(r.Bucket),
 		Key:          aws.String(filename),
 		Body:         file,
