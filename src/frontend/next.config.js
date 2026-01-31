e4168a9e9be4b7ab832c2bfc2fb5d22be5f851d0
Stanislav Rush <i@svrush.ru>
2026-02-01 03:40:29 +0700

    [CI/CD] Настроена отправка source map в Sentry Resolve #939

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -8,22 +8,27 @@ const nextConfig = {
 };
 
 
-module.exports = nextConfig
-
-
-// Injected content via Sentry wizard below
-
 const { withSentryConfig } = require("@sentry/nextjs");
 
+// В формате sentryUrl;org;project
+SENTRY_CONF = process.env.SENTRY_CONF.split(";")
+
+
+
 module.exports = withSentryConfig(
-  module.exports,
-  {
+    nextConfig,
+    {
     silent: !process.env.CI,
     widenClientFileUpload: true,
     reactComponentAnnotation: {
       enabled: true,
     },
+    sentryUrl: SENTRY_CONF[0],
     hideSourceMaps: true,
     disableLogger: true,
+    org: SENTRY_CONF[1],
+    project: SENTRY_CONF[2],
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    release: process.env.NEXT_PUBLIC_VERSION,
   }
 );
