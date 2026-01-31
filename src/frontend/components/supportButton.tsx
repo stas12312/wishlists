0f44b21dd8a2f3e766ded355af7afc1a28776168
Stanislav Rush <i@svrush.ru>
2026-02-01 01:02:33 +0700

    [UI] Добавлена кнопка "Поддержать проект" Resolve #930

---
+++ src/frontend/components/supportButton.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@heroui/button";
+import { IoMdHeart } from "react-icons/io";
+import Link from "next/link";
+
+export const SupportButton = ({ className = "" }: { className?: string }) => {
+  return (
+    <Button
+      as={Link}
+      className={`bg-gradient-to-r dark:from-amber-800 dark:to-amber-700 from-amber-400 to-amber-300 shadow-sm ${className}`}
+      href={process.env.NEXT_PUBLIC_DONATE_URL}
+      startContent={<IoMdHeart className="text-rose-500 dark:text-rose-400" />}
+      target="_blank"
+      variant="light"
+    >
+      Поддержать проект
+    </Button>
+  );
+};
