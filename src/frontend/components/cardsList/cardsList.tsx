0f5958042676769232f63ff58125c7b291b031e9
Stanislav Rush <i@svrush.ru>
2026-02-03 02:09:45 +0700

    [UI] Исправлены анимации появления карточек Resolve #962

--- src/frontend/components/cardsList/cardsList.tsx
+++ src/frontend/components/cardsList/cardsList.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { observer } from "mobx-react-lite";
 import { ReactElement } from "react";
 
@@ -10,26 +10,33 @@ export const CardsList = observer(
       <motion.div
         animate="show"
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 col-span-full"
-        exit="exit"
         initial="hidden"
         variants={containerVariants}
       >
-        <AnimatePresence mode="popLayout">
-          {items.map((item) => {
-            return item ? (
-              <motion.div
-                key={item.key}
-                layout
-                animate="show"
-                exit={{ opacity: 0, y: -20, scale: 0.95 }}
-                initial="hidden"
-                variants={itemVariants}
-              >
-                {item}
-              </motion.div>
-            ) : null;
-          })}
-        </AnimatePresence>
+        {items.map((item) => {
+          if (!item) {
+            return null;
+          }
+          return (
+            <motion.div
+              key={item.key}
+              layout
+              animate={{
+                opacity: 1,
+                transform: "translateY(0px)",
+                transition: { duration: 0.3 },
+              }}
+              exit={{
+                opacity: 0,
+                transform: "translateY(-20px)",
+                transition: { duration: 0.1 },
+              }}
+              variants={itemVariants}
+            >
+              {item}
+            </motion.div>
+          );
+        })}
       </motion.div>
     );
   },
