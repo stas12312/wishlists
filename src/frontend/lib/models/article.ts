b53c45c36986f56e0345bf6bcb59f351e82a6759
Stanislav Rush <i@svrush.ru>
2026-02-24 22:42:51 +0700

    [UI] Исправлено создание статьи (#1018)
    Resolve #1017

--- src/frontend/lib/models/article.ts
+++ src/frontend/lib/models/article.ts
@@ -5,10 +5,9 @@ export interface IArticle {
   title: string;
   description: string;
   content?: OutputData;
-  create_at?: string;
   is_published: boolean;
   published_at?: string;
-  created_at: string;
+  created_at?: string;
   image: string;
   slug: string;
 }
