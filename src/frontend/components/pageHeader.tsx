c6376e0fede085b3710ec257b59049b09136ea3a
Stanislav Rush <i@svrush.ru>
2026-02-27 00:59:24 +0700

    [UI] Исправлено отображение лишнего h1 (#1033)
    Resolve #1024

--- src/frontend/components/pageHeader.tsx
+++ src/frontend/components/pageHeader.tsx
@@ -12,7 +12,9 @@ const PageHeader = ({
 }) => {
   return (
     <div className="flex flex-col">
-      <h1 className="text-2xl text-center md:text-left">{title}</h1>
+      {title ? (
+        <h1 className="text-2xl text-center md:text-left">{title}</h1>
+      ) : null}
       {children}
       <Divider className={dividerClassName} />
     </div>
