3d06055d5e53918333adb7fbf19b275d58bd27e3
Stanislav Rush <i@svrush.ru>
2026-03-19 21:18:19 +0700

    [UI] Исправлено видимость формы вопросов (#1086)
    Resolve #1084

--- src/frontend/components/wish/fullCard.tsx
+++ src/frontend/components/wish/fullCard.tsx
@@ -9,6 +9,7 @@ import { Button } from "@heroui/button";
 import { Link } from "@heroui/link";
 import { Chip } from "@heroui/chip";
 import { Avatar } from "@heroui/avatar";
+import { observer } from "mobx-react-lite";
 
 import MarketIcon from "../marketIcon";
 import { QuestionList } from "../questions/list";
@@ -18,105 +19,108 @@ import { ImageSwiper } from "./swiper";
 import { IWish } from "@/lib/models/wish";
 import { getMenuItemsByActions } from "@/lib/wish/menu";
 import { getUserLink } from "@/lib/label";
+import userStore from "@/store/userStore";
 
-const WishFullCard = ({
-  isOpen,
-  onOpenChange,
-  handeAction,
-  wish,
-  withUser = false,
-}: {
-  isOpen: boolean;
-  onOpenChange: { (): void };
-  handeAction: { (action: string): void };
-  wish: IWish;
-  withUser?: boolean;
-}) => {
-  const menuItems = getMenuItemsByActions(wish.actions);
-  return (
-    <Drawer isOpen={isOpen} size="xl" onOpenChange={onOpenChange}>
-      <DrawerContent>
-        {(onClose) => (
-          <>
-            <DrawerHeader className="text-center">
-              <h1 className="text-2xl">{wish.name}</h1>
-            </DrawerHeader>
+const WishFullCard = observer(
+  ({
+    isOpen,
+    onOpenChange,
+    handeAction,
+    wish,
+    withUser = false,
+  }: {
+    isOpen: boolean;
+    onOpenChange: { (): void };
+    handeAction: { (action: string): void };
+    wish: IWish;
+    withUser?: boolean;
+  }) => {
+    const menuItems = getMenuItemsByActions(wish.actions);
+    return (
+      <Drawer isOpen={isOpen} size="xl" onOpenChange={onOpenChange}>
+        <DrawerContent>
+          {(onClose) => (
+            <>
+              <DrawerHeader className="text-center">
+                <h1 className="text-2xl">{wish.name}</h1>
+              </DrawerHeader>
 
-            <DrawerBody className="flex">
-              {withUser ? (
-                <div className="mx-auto">
-                  <Link
-                    className="md:hover:scale-[1.03] transition"
-                    href={getUserLink(wish.user.username)}
-                  >
-                    <Chip avatar={<Avatar src={wish.user.image} />}>
-                      {wish.user.name}
-                    </Chip>
-                  </Link>
-                </div>
-              ) : null}
+              <DrawerBody className="flex">
+                {withUser ? (
+                  <div className="mx-auto">
+                    <Link
+                      className="md:hover:scale-[1.03] transition"
+                      href={getUserLink(wish.user.username)}
+                    >
+                      <Chip avatar={<Avatar src={wish.user.image} />}>
+                        {wish.user.name}
+                      </Chip>
+                    </Link>
+                  </div>
+                ) : null}
 
-              <span className="text-default-500 whitespace-pre-wrap">
-                {wish.comment}
-              </span>
-
-              {wish.link ? (
-                <Link
-                  isBlock
-                  isExternal
-                  showAnchorIcon
-                  className="w-full"
-                  href={wish.link}
-                  size="lg"
-                >
-                  <MarketIcon link={wish.link || ""} />
-
-                  <span className="mx-auto">Перейти в магазин</span>
-                </Link>
-              ) : null}
-
-              <ImageSwiper wish={wish} />
-
-              <div className="flex flex-col gap-2">
-                {menuItems.map((value) => (
-                  <Button
-                    key={value.key}
-                    color={value.color}
-                    startContent={<value.icon />}
-                    variant="flat"
-                    onPress={() => {
-                      handeAction(value.key);
-                    }}
-                  >
-                    {value.getTitleFunc !== undefined
-                      ? value.getTitleFunc(wish.actions)
-                      : value.title}
-                  </Button>
-                ))}
-              </div>
-              {wish.uuid ? (
-                <QuestionList
-                  withTitle
-                  wishUUID={wish.uuid}
-                  withAskForm={!wish.actions.edit}
-                />
-              ) : null}
-            </DrawerBody>
-            <DrawerFooter className="flex flex-col">
-              {wish.created_at ? (
-                <span className="text-default-500">
-                  Добавлено {new Date(wish.created_at).toLocaleString()}
+                <span className="text-default-500 whitespace-pre-wrap">
+                  {wish.comment}
                 </span>
-              ) : null}
-              <Button fullWidth color="danger" onPress={onClose}>
-                Закрыть
-              </Button>
-            </DrawerFooter>
-          </>
-        )}
-      </DrawerContent>
-    </Drawer>
-  );
-};
+
+                {wish.link ? (
+                  <Link
+                    isBlock
+                    isExternal
+                    showAnchorIcon
+                    className="w-full"
+                    href={wish.link}
+                    size="lg"
+                  >
+                    <MarketIcon link={wish.link || ""} />
+
+                    <span className="mx-auto">Перейти в магазин</span>
+                  </Link>
+                ) : null}
+
+                <ImageSwiper wish={wish} />
+
+                <div className="flex flex-col gap-2">
+                  {menuItems.map((value) => (
+                    <Button
+                      key={value.key}
+                      color={value.color}
+                      startContent={<value.icon />}
+                      variant="flat"
+                      onPress={() => {
+                        handeAction(value.key);
+                      }}
+                    >
+                      {value.getTitleFunc !== undefined
+                        ? value.getTitleFunc(wish.actions)
+                        : value.title}
+                    </Button>
+                  ))}
+                </div>
+                {wish.uuid ? (
+                  <QuestionList
+                    withTitle
+                    wishUUID={wish.uuid}
+                    withAskForm={!wish.actions.edit && userStore.user.id > 0}
+                  />
+                ) : null}
+              </DrawerBody>
+              <DrawerFooter className="flex flex-col">
+                {wish.created_at ? (
+                  <span className="text-default-500">
+                    Добавлено {new Date(wish.created_at).toLocaleString()}
+                  </span>
+                ) : null}
+                <Button fullWidth color="danger" onPress={onClose}>
+                  Закрыть
+                </Button>
+              </DrawerFooter>
+            </>
+          )}
+        </DrawerContent>
+      </Drawer>
+    );
+  },
+);
 
 export default WishFullCard;
