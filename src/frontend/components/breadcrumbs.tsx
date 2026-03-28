34c190d66829c02680246f6b08e3f3cc0d33649d
Stanislav Rush <i@svrush.ru>
2026-03-28 19:31:38 +0700

    [UI] Исправлено отображение длинного названия Resolve #1151

--- src/frontend/components/breadcrumbs.tsx
+++ src/frontend/components/breadcrumbs.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Breadcrumbs, BreadcrumbsItem } from "@heroui/react";
+import clsx from "clsx";
 import { useRouter } from "next/navigation";
 
 interface IBreadcrumbItem {
@@ -9,22 +10,25 @@ interface IBreadcrumbItem {
 
 export const CustomBreadcrumbs = ({ items }: { items: IBreadcrumbItem[] }) => {
   const router = useRouter();
+  const itemClassName = clsx(
+    "text-2xl  line-clamp-3 md:line-clamp-1 text-center md:text-left overflow-hidden",
+  );
   return (
-    <Breadcrumbs className="justify justify-center md:justify-start">
+    <Breadcrumbs className="justify justify-center md:justify-start flex-col md:flex-row">
       {items.map((item, i) => (
         <BreadcrumbsItem
           key={item.title}
-          className="*:p-0 p-0"
+          className="*:p-0 p-0 shrink"
           onPress={() => {
             router.push(item.href);
           }}
         >
           {i == items.length - 1 ? (
-            <h1 className="text-2xl line-clamp-2 whitespace-normal text-center md:text-left">
+            <h1 className={itemClassName} title={item.title}>
               {item.title}
             </h1>
           ) : (
-            <span className="text-2xl line-clamp-2 whitespace-normal text-center md:text-left">
+            <span className={itemClassName} title={item.title}>
               {item.title}
             </span>
           )}
